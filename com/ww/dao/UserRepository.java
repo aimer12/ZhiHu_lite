8f60841409bad2c0f0798016f9fe42031e249e93
Aimer <aimer12@users.noreply.github.com>
2017-02-06 10:21:05 +0800

    Create UserRepository.java

---
+++ com/ww/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ww.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created on 2016/11/3.
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
