42463e0a084e5fa351c95fc765ad46dd137775bc
Aimer <aimer12@users.noreply.github.com>
2017-02-06 10:36:59 +0800

    Create data.sql

---
+++ resources/data.sql
@@ -0,0 +1,22 @@
+insert into USER (id, name) values(1, 'aimer');
+
+insert into Article (id, title, context, auther, jiang, create_time) values (1,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (2,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (3,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (4,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (5,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (6,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (7,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (8,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (9,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (10,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (11,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (12,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (13,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (14,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (15,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (16,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (17,'title','context',1 ,1, '2012-06-18 10:34:09');
+insert into Article (id, title, context, auther, jiang, create_time) values (18,'title','context',1 ,1, '2012-06-18 10:34:09');
+
+insert into Jiang (id, auid, arid) values (1,1,1);
