de16ffbffbcd612a384e8a1048f32ecbc6d79c07
Aimer <aimer12@users.noreply.github.com>
2017-02-06 10:34:00 +0800

    Create Application.java

---
+++ java/com/ww/Application.java
@@ -0,0 +1,15 @@
+package com.ww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@Configuration
+//@EnableAutoConfiguration
+//@ComponentScan
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
